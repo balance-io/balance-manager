04a0049ca396855927ed771fdd5227a12fe197c5
Pedro Gomes <pedrogomes94@gmail.com>
2018-03-15 20:08:38 -0500

    add native currency dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,51 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import CryptoIcon from '../components/CryptoIcon';
-import selector from '../assets/selector-grey.svg';
-import { convertToNativeString } from '../helpers/utilities';
+import caret from '../assets/caret.svg';
 import { fonts, colors, shadows, responsive } from '../styles';
 
 const StyledWrapper = styled.div`
   width: 100%;
   z-index: 2;
-  border-radius: ${({ show }) => (show ? '6px 6px 0 0' : '6px')};
   position: relative;
   -webkit-box-shadow: ${shadows.medium};
   box-shadow: ${shadows.medium};
 `;
 
-const StyledAsset = styled.div`
-  display: flex;
-  align-items: center;
-  text-align: right;
-  & p {
-    color: rgb(${colors.dark});
-    font-size: ${fonts.size.medium};
-    font-weight: ${fonts.size.semibold};
-    margin-left: 10px;
-  }
-  @media screen and (${responsive.xs.max}) {
-    & p {
-      font-size: ${fonts.size.small} !important;
-    }
-  }
+const StyledCaret = styled.img`
+  position: absolute;
+  right: 7px;
+  top: calc(50% - 7px);
+  width: 14px;
+  height: 14px;
+  mask: url(${caret}) center no-repeat;
+  mask-size: 90%;
+  background-color: rgba(${colors.white}, 0.8);
+`;
+
+const StyledIcon = styled.div`
+  height: 15px;
+  width: 15px;
+  mask: ${({ icon }) => (icon ? `url(${icon}) center no-repeat` : 'none')};
+  mask-size: 60%;
+  background-color: rgba(${colors.white}, 0.8);
 `;
 
 const StyledRow = styled.div`
   border-radius: 6px;
-  color: rgb(${colors.grey});
-  background: rgb(${colors.white});
+  position: relative;
+  color: rgba(${colors.white}, 0.8);
   font-size: ${fonts.size.small};
   font-weight: ${fonts.weight.medium};
   text-align: center;
   outline: none;
   & > div {
-    padding: ${({ noOptions }) => (noOptions ? `10px` : `10px 26px 10px 10px`)};
+    padding: ${({ noOptions }) => (noOptions ? `8px` : `8px 26px 8px 8px`)};
     background-size: 8px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+  }
+  & p {
+    margin: 0 4px;
   }
   @media screen and (${responsive.xs.max}) {
     & p {
@@ -55,17 +57,20 @@ const StyledRow = styled.div`
 `;
 
 const StyledSelected = styled(StyledRow)`
-  background: ${({ noOptions }) =>
-    noOptions ? `rgb(${colors.white})` : `rgb(${colors.white}) url(${selector})`};
-  background-size: 8px;
-  background-position: 98% 50%;
-  background-repeat: no-repeat;
   outline: none;
+  border: 1px solid rgba(${colors.white}, 0.1);
+  border-radius: ${({ show }) => (show ? '6px 6px 0 0' : '6px')};
+  & ${StyledCaret} {
+    opacity: ${({ noOptions }) => (noOptions ? 0 : 1)};
+  }
 `;
 
 const StyledDropdown = styled(StyledRow)`
   position: absolute;
+  background: rgb(${colors.white});
+  color: rgb(${colors.grey});
   border-radius: 0 0 6px 6px;
+  border: 1px solid rgba(${colors.white});
   width: 100%;
   top: 100%;
   opacity: ${({ show }) => (show ? 1 : 0)};
@@ -76,11 +81,14 @@ const StyledDropdown = styled(StyledRow)`
   & > div {
     border-top: 1px solid rgba(${colors.lightGrey}, 0.7);
   }
+  & ${StyledIcon} {
+    background-color: rgb(${colors.grey});
+  }
 `;
 
-class DropdownCrypto extends Component {
+class Dropdown extends Component {
   state = {
-    selected: 'ETH',
+    selected: this.props.options[Object.keys(this.props.options)[0]].value,
     showDropdown: false
   };
   onChangeSelected = selected => {
@@ -89,53 +97,30 @@ class DropdownCrypto extends Component {
   };
   toggleDropdown = () => this.setState({ showDropdown: !this.state.showDropdown });
   render() {
-    const { selected, ethBalance, tokens, ...props } = this.props;
-    let options = {
-      ETH: {
-        name: 'Ethereum',
-        symbol: 'ETH',
-        balance: ethBalance
-      }
-    };
-    if (tokens) {
-      let parsedTokens = {};
-      tokens.forEach(token => {
-        parsedTokens[token.symbol] = token;
-      });
-      options = { ...options, ...parsedTokens };
-    }
+    const { options, ...props } = this.props;
     return (
-      <StyledWrapper show={this.state.showDropdown} {...props}>
-        <StyledSelected noOptions={Object.keys(options).length < 2} onClick={this.toggleDropdown}>
+      <StyledWrapper {...props}>
+        <StyledSelected
+          show={this.state.showDropdown}
+          noOptions={Object.keys(options).length < 2}
+          onClick={this.toggleDropdown}
+        >
           <div>
-            <StyledAsset>
-              <CryptoIcon size={18} currency={options[this.state.selected].symbol} />
-              <p>{options[this.state.selected].name}</p>
-            </StyledAsset>
-            <p>{`${options[this.state.selected].balance} ${
-              options[this.state.selected].symbol
-            } ≈ ${convertToNativeString(
-              options[this.state.selected].balance,
-              options[this.state.selected].symbol
-            )}`}</p>
+            <StyledIcon icon={options[this.state.selected].icon} />
+            <p>{options[this.state.selected].value}</p>
           </div>
+          <StyledCaret />
         </StyledSelected>
         <StyledDropdown show={this.state.showDropdown}>
           {Object.keys(options)
             .filter(key => key !== this.state.selected)
             .map(key => (
               <div
-                key={options[key].symbol}
-                onClick={() => this.onChangeSelected(options[key].symbol)}
+                key={options[key].value}
+                onClick={() => this.onChangeSelected(options[key].value)}
               >
-                <StyledAsset>
-                  <CryptoIcon size={18} currency={options[key].symbol} />
-                  <p>{options[key].name}</p>
-                </StyledAsset>
-                <p>{`${options[key].balance} ${options[key].symbol} ≈ ${convertToNativeString(
-                  options[key].balance,
-                  options[key].symbol
-                )}`}</p>
+                <StyledIcon icon={options[key].icon} />
+                <p>{options[key].value}</p>
               </div>
             ))}
         </StyledDropdown>
@@ -144,10 +129,9 @@ class DropdownCrypto extends Component {
   }
 }
 
-DropdownCrypto.propTypes = {
-  selected: PropTypes.string.isRequired,
-  tokens: PropTypes.array.isRequired,
-  ethBalance: PropTypes.string.isRequired
+Dropdown.propTypes = {
+  options: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired
 };
 
-export default DropdownCrypto;
+export default Dropdown;
--- src/layouts/base.js
+++ src/layouts/base.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 import Link from '../components/Link';
-import logo from '../assets/logo-light.png';
 import Modal from '../components/Modal';
+import Dropdown from '../components/Dropdown';
 import Background from '../components/Background';
 import IconPreload from '../components/IconPreload';
 import Wrapper from '../components/Wrapper';
 import Column from '../components/Column';
 import Notification from '../components/Notification';
+import logo from '../assets/logo-light.png';
+import iconUSD from '../assets/usd.svg';
+import iconGBP from '../assets/gbp.svg';
+import iconEUR from '../assets/eur.svg';
+import iconBTC from '../assets/btc.svg';
+import iconETH from '../assets/eth.svg';
+import { accountsChangeNativeCurrency } from '../reducers/_accounts';
 import { fonts, responsive } from '../styles';
 
 const StyledLayout = styled.div`
@@ -57,7 +65,36 @@ const StyledLogo = styled.img`
   width: 20px;
 `;
 
-const BaseLayout = ({ children, ...props }) => (
+const StyledToolbar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const nativeDropdown = {
+  USD: {
+    value: 'USD',
+    icon: iconUSD
+  },
+  GBP: {
+    value: 'GBP',
+    icon: iconGBP
+  },
+  EUR: {
+    value: 'EUR',
+    icon: iconEUR
+  },
+  BTC: {
+    value: 'BTC',
+    icon: iconBTC
+  },
+  ETH: {
+    value: 'ETH',
+    icon: iconETH
+  }
+};
+
+const BaseLayout = ({ children, accountsChangeNativeCurrency, ...props }) => (
   <StyledLayout>
     <Background />
     <IconPreload />
@@ -71,9 +108,9 @@ const BaseLayout = ({ children, ...props }) => (
             </StyledHero>
           </StyledBranding>
         </Link>
-        {/* <StyledToolbar>
-              <Dropdown />
-            </StyledToolbar> */}
+        <StyledToolbar>
+          <Dropdown options={nativeDropdown} onChange={accountsChangeNativeCurrency} />
+        </StyledToolbar>
       </StyledHeader>
       <StyledContent>{children}</StyledContent>
     </Column>
@@ -83,7 +120,10 @@ const BaseLayout = ({ children, ...props }) => (
 );
 
 BaseLayout.propTypes = {
+  accountsChangeNativeCurrency: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired
 };
 
-export default BaseLayout;
+export default connect(null, {
+  accountsChangeNativeCurrency
+})(BaseLayout);

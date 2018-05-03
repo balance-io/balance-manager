8b03fb83d4dfefbb20986cd60cfa45775924e8af
jinchung <jin.chung17@gmail.com>
2018-05-03 15:40:22 -0400

    missing s

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -222,7 +222,7 @@ class Home extends Component {
 
       <StyledCard>
         <StyledCardContainer>
-          <StyledHardwareWallet>
+          <StyledHardwareWallets>
             <StyledLedgerLogo />
             <p>Connect and sign with your Ledger hardware wallet.</p>
             <Link to="/ledger">
@@ -230,7 +230,7 @@ class Home extends Component {
                 {lang.t('button.connect_ledger')}
               </StyledLedgerButton>
             </Link>
-          </StyledHardwareWallet>
+          </StyledHardwareWallets>
         </StyledCardContainer>
       </StyledCard>
 

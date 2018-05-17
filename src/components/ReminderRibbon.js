0ff065b1190c08a9e37eea94e893faa65354856f
Mike Keen <mkeen.atl@gmail.com>
2018-05-17 18:04:51 -0400

    New references to localstorage utils

--- src/components/ReminderRibbon.js
+++ src/components/ReminderRibbon.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { saveLocal, getLocal } from '../helpers/utilities';
+import { saveLocal, getLocal } from '../handlers/localstorage';
 
 const StyledReminderRibbon = styled.div`
   will-change: height;

facae05bea0078b8ecebd929e300f014ea796a55
Pedro Gomes <pedrogomes94@gmail.com>
2018-05-22 17:29:42 -0400

    tweak

--- src/views/Account/AccountTransactions.js
+++ src/views/Account/AccountTransactions.js
@@ -233,9 +233,15 @@ class AccountViewTransactions extends Component {
     this.setState({ showAllTransactions: !this.state.showAllTransactions });
 
   render = () => {
-    const nativeCurrency = this.props.nativeCurrency;
-    return !!this.props.transactions.length ? (
-      !this.props.fetchingTransactions ? (
+    const {
+      transactions,
+      fetchingTransactions,
+      network,
+      accountAddress,
+      nativeCurrency,
+    } = this.props;
+    return !!transactions.length ? (
+      !fetchingTransactions ? (
         <StyledGrid>
           <StyledLabelsRow>
             <StyledLabels>{lang.t('account.label_asset')}</StyledLabels>
@@ -245,7 +251,7 @@ class AccountViewTransactions extends Component {
             <StyledLabels>{lang.t('account.label_total')}</StyledLabels>
           </StyledLabelsRow>
 
-          {this.props.transactions.map((tx, idx, arr) => {
+          {transactions.map((tx, idx, arr) => {
             if (!this.state.showAllTransactions && idx > 10) return null;
             return (
               <StyledTransactionWrapper
@@ -268,11 +274,11 @@ class AccountViewTransactions extends Component {
                     </StyledAsset>
                     <TransactionStatus
                       tx={tx}
-                      accountAddress={this.props.accountAddress}
+                      accountAddress={accountAddress}
                     />
 
                     <p>
-                      {tx.from === this.props.accountAddress
+                      {tx.from === accountAddress
                         ? `- ${tx.value.display}`
                         : `${tx.value.display}`}
                     </p>
@@ -287,7 +293,7 @@ class AccountViewTransactions extends Component {
                       {tx.native &&
                       tx.native[nativeCurrency] &&
                       tx.native[nativeCurrency].value
-                        ? tx.from === this.props.accountAddress
+                        ? tx.from === accountAddress
                           ? `- ${tx.native[nativeCurrency].value.display}`
                           : `${tx.native[nativeCurrency].value.display}`
                         : '———'}
@@ -298,27 +304,19 @@ class AccountViewTransactions extends Component {
                   >
                     <div>
                       <StyledBlockie
-                        seed={
-                          tx.from === this.props.accountAddress
-                            ? tx.to
-                            : tx.from
-                        }
+                        seed={tx.from === accountAddress ? tx.to : tx.from}
                       />
                       <div>
                         <p>
                           <strong>
                             {tx.from === tx.to
                               ? lang.t('account.tx_self').toUpperCase()
-                              : tx.from === this.props.accountAddress
+                              : tx.from === accountAddress
                                 ? lang.t('account.tx_to').toUpperCase()
                                 : lang.t('account.tx_from').toUpperCase()}
                           </strong>
                         </p>
-                        <p>
-                          {tx.from === this.props.accountAddress
-                            ? tx.to
-                            : tx.from}
-                        </p>
+                        <p>{tx.from === accountAddress ? tx.to : tx.from}</p>
                       </div>
                     </div>
                     <div>
@@ -369,9 +367,7 @@ class AccountViewTransactions extends Component {
                     <div>
                       <a
                         href={`https://${
-                          this.props.network !== 'mainnet'
-                            ? `${this.props.network}.`
-                            : ''
+                          network !== 'mainnet' ? `${network}.` : ''
                         }etherscan.io/tx/${tx.hash.replace(/-.*/g, '')}`}
                         target="_blank"
                         rel="noreferrer noopener"
@@ -394,7 +390,7 @@ class AccountViewTransactions extends Component {
                       >
                         <ButtonCustom
                           left
-                          disabled={this.props.network !== 'mainnet'}
+                          disabled={network !== 'mainnet'}
                           txtColor="ethplorer"
                           img={ethplorerLogo}
                         >
@@ -414,7 +410,7 @@ class AccountViewTransactions extends Component {
               </StyledTransactionWrapper>
             );
           })}
-          {this.props.transactions.length > 10 && (
+          {transactions.length > 10 && (
             <StyledShowAllTransactions onClick={this.onShowAllTransactions}>
               <StyledToggleIndicator show={this.state.showAllTransactions} />
               <p>
@@ -428,12 +424,12 @@ class AccountViewTransactions extends Component {
           )}
         </StyledGrid>
       ) : (
-        <StyledCard minHeight={280} fetching={this.props.fetchingTransactions}>
+        <StyledCard minHeight={280} fetching={fetchingTransactions}>
           <StyledMessage>{lang.t('message.failed_request')}</StyledMessage>
         </StyledCard>
       )
     ) : (
-      <StyledCard minHeight={280} fetching={this.props.fetchingTransactions}>
+      <StyledCard minHeight={280} fetching={fetchingTransactions}>
         <StyledMessage>{lang.t('message.no_transactions')}</StyledMessage>
       </StyledCard>
     );

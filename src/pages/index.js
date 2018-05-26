b5024075dc7c298a4b4dce1aa31b0182b5c4f606
Pedro Gomes <pedrogomes94@gmail.com>
2018-05-25 22:39:41 -0300

    tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -140,24 +140,9 @@ const TrezorLogo = styled.div`
   width: 109px;
 `;
 
-const StyledWalletConnect = styled.div`
-  & p {
-    margin: 43px 20px 0 196px;
-  }
-
-  @media screen and (max-width: 736px) {
-    & p {
-      display: none;
-    }
-  }
-`;
-
 const StyledWalletConnectLogo = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 173px;
-  height: 102px;
+  margin: 8px 0;
+  height: 54px;
   background: url(${walletConnectLogoImage});
   background-size: contain;
   background-repeat: no-repeat;
@@ -236,17 +221,17 @@ class Home extends Component {
 
       <StyledCard>
         <CardContainer>
-          <StyledWalletConnect>
+          <LogoSection>
             <StyledWalletConnectLogo />
-            <p>{lang.t('homepage.connect_walletconnect')}</p>
-            <StyledWalletConnectButton
-              left
-              color="walletconnect"
-              onClick={this.onWalletConnectInit}
-            >
-              {lang.t('button.connect_walletconnect')}
-            </StyledWalletConnectButton>
-          </StyledWalletConnect>
+            <LogoText>{lang.t('homepage.connect_walletconnect')}</LogoText>
+          </LogoSection>
+          <StyledWalletConnectButton
+            left
+            color="walletconnect"
+            onClick={this.onWalletConnectInit}
+          >
+            {lang.t('button.connect_walletconnect')}
+          </StyledWalletConnectButton>
         </CardContainer>
       </StyledCard>
     </BaseLayout>

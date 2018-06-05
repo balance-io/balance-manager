ddc6f087fc8ef4f861b2a344a609ee2e650130b0
Michel Gotta <michel@michelgotta.de>
2018-06-05 05:39:42 +0200

    Provided german translation

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,10 +1,12 @@
 import i18next from 'i18next';
 import english from './_english.json';
 import spanish from './_spanish.json';
+import spanish from './_german.json';
 
 export const resources = {
   en: english,
   es: spanish,
+  de: german,
 };
 
 export const updateLanguage = code => i18next.changeLanguage(code);

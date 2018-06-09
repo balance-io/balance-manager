79a16be559ccf9780055a0730cde98ca0f5fadd0
Corrado Verhoeven <corra98@gmail.com>
2018-06-09 16:24:03 +0200

    Fix index.js
    Added supporto for italian language in index.js

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,10 +1,12 @@
 import i18next from 'i18next';
 import english from './_english.json';
 import spanish from './_spanish.json';
+import italian from './_italian.json';
 
 export const resources = {
   en: english,
   es: spanish,
+  it: italian,
 };
 
 export const updateLanguage = code => i18next.changeLanguage(code);

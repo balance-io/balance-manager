8bf1288b0e51d8eb2008b3d18af6538872f67adf
Leo Osa <whoisthescientist@gmail.com>
2018-06-11 22:26:21 +0300

    add russian to dropdown

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,10 +1,12 @@
 import i18next from 'i18next';
 import english from './_english.json';
 import spanish from './_spanish.json';
+import russian from './_russian.json';
 
 export const resources = {
   en: english,
   es: spanish,
+  ru: russian,
 };
 
 export const updateLanguage = code => i18next.changeLanguage(code);

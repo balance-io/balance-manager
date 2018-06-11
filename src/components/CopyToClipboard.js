33cfdf417352c3762615880c946564676f4139a6
gokulkrishh <krishnangokul9@gmail.com>
2018-06-11 17:02:30 +0530

    Added copied confirmation with text + prettier fixes

--- src/components/CopyToClipboard.js
+++ src/components/CopyToClipboard.js
@@ -3,20 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import lang from '../languages';
-import clipboardIcon from '../assets/clipboard.png';
+import clipboardIcon from '../assets/copy-icon.svg';
 import { toChecksumAddress } from '../handlers/web3';
 import { notificationShow } from '../reducers/_notification';
 import { fonts, colors, transitions, responsive } from '../styles';
 
 const StyledIcon = styled.img`
-  cursor: pointer;
-  transition: ${transitions.base};
-  position: absolute;
-  right: -20px;
-  top: calc(50% - 8px);
-  width: 16px;
-  height: 16px;
-  opacity: 0;
+  width: 11px;
+  height: 11px;
 `;
 
 const StyledText = styled.p`
@@ -31,6 +25,37 @@ const StyledText = styled.p`
   letter-spacing: -0.2px;
 `;
 
+const StyledCopyTextWidth = 68;
+const StyledCopyText = styled.button`
+  transition: all 200ms ease-in-out;
+  font-weight: ${fonts.weight.medium};
+  font-size: ${fonts.size.small};
+  font-family: ${fonts.family.SFMono};
+  padding: 3px;
+  border-radius: 4px;
+  background-color: ${({ copied }) => (copied ? '#26b881' : '#3f51b5')};
+  color: #fff;
+  width: ${StyledCopyTextWidth}px;
+  height: 24px;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  right: -${StyledCopyTextWidth - 2}px;
+  top: 0;
+  cursor: pointer;
+  transition: ${transitions.base};
+  user-select: none;
+
+  &:active {
+    opacity: 0.9;
+  }
+
+  &:hover {
+    opacity: 0.95;
+  }
+`;
+
 const StyledCopyToClipboard = styled.div`
   width: 100%;
   min-height: 19px;
@@ -40,14 +65,6 @@ const StyledCopyToClipboard = styled.div`
 const StyledContainer = styled.div`
   position: relative;
   display: inline;
-  @media (hover: hover) {
-    &:hover ${StyledIcon} {
-      opacity: ${({ iconOnHover }) => (iconOnHover ? '1' : '0')};
-    }
-    &:hover ${StyledText} {
-      opacity: ${({ iconOnHover }) => (iconOnHover ? '0' : '0.7')};
-    }
-  }
 `;
 
 const StyledInputText = styled.input`
@@ -66,6 +83,9 @@ const StyledInputText = styled.input`
   @media screen and (${responsive.sm.max}) {
     font-size: ${fonts.size.small};
   }
+  background-color: #e2e0e0;
+  border-radius: 2px;
+  padding: 3px 5px;
 `;
 
 const StyledInput = styled(StyledInputText)`
@@ -92,11 +112,16 @@ const StyledInvisible = styled(StyledInputParagraph)`
 let timeout = null;
 
 class CopyToClipboard extends Component {
+  state = {
+    copied: false,
+  };
+  timeoutHide = null;
   copyToCopyToClipboard = ({ target }) => {
     clearTimeout(timeout);
     target.select();
     document.execCommand('Copy');
     target.blur();
+    this.onCopyButtonHandler();
     this.props.notificationShow(
       lang.t('notification.info.address_copied_to_clipboard'),
     );
@@ -109,16 +134,25 @@ class CopyToClipboard extends Component {
     element.select();
     document.execCommand('copy');
     document.body.removeChild(element);
+    this.onCopyButtonHandler();
     this.props.notificationShow(
       lang.t('notification.info.address_copied_to_clipboard'),
     );
   };
+
+  onCopyButtonHandler = () => {
+    if (this.timeoutHide) clearTimeout(this.timeoutHide);
+    this.setState({ copied: true });
+    this.timeoutHide = setTimeout(() => this.setState({ copied: false }), 3000);
+  };
+
   render() {
-    let { notificationShow, iconOnHover, text, ...props } = this.props;
+    let { notificationShow, text, ...props } = this.props;
+    let { copied } = this.state;
     text = toChecksumAddress(text);
     return (
       <StyledCopyToClipboard {...props}>
-        <StyledContainer iconOnHover={iconOnHover}>
+        <StyledContainer>
           <StyledInvisible>{text}</StyledInvisible>
           <StyledInput
             value={text}
@@ -129,11 +163,13 @@ class CopyToClipboard extends Component {
           <StyledText>
             {lang.t('message.click_to_copy_to_clipboard')}
           </StyledText>
-          <StyledIcon
-            src={clipboardIcon}
-            alt="copy"
+          <StyledCopyText
             onClick={this.simulateCopyToClipboard}
-          />
+            copied={copied}
+          >
+            <StyledIcon src={clipboardIcon} alt="copy" />
+            {copied ? lang.t('message.copied') : lang.t('message.copy')}
+          </StyledCopyText>
         </StyledContainer>
       </StyledCopyToClipboard>
     );
@@ -143,11 +179,6 @@ class CopyToClipboard extends Component {
 CopyToClipboard.propTypes = {
   notificationShow: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
-  iconOnHover: PropTypes.bool,
-};
-
-CopyToClipboard.defaultProps = {
-  iconOnHover: false,
 };
 
 export default connect(null, { notificationShow })(CopyToClipboard);

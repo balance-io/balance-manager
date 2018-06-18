60dbec9e67800eb012a1bf86471177be9f50c7e5
Jetpack <j3tp4ck@protonmail.ch>
2018-06-18 13:09:13 +0200

    Add czech language support

--- src/languages/index.js
+++ src/languages/index.js
@@ -4,6 +4,7 @@ import spanish from './_spanish.json';
 import russian from './_russian.json';
 import italian from './_italian.json';
 import german from './_german.json';
+import czech from './_czech.json';
 
 export const resources = {
   en: english,
@@ -11,6 +12,7 @@ export const resources = {
   ru: russian,
   it: italian,
   de: german,
+  cz: czech,
 };
 
 export const updateLanguage = code => i18next.changeLanguage(code);

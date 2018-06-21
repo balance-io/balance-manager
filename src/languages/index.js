120cdb652fedc4e19b000a2c73437825310af74a
George <george@abstractpoint.co.uk>
2018-06-21 11:12:59 +0100

    #276 Brzilian language translation

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,16 +1,18 @@
 import i18next from 'i18next';
 import english from './_english.json';
-import spanish from './_spanish.json';
-import russian from './_russian.json';
-import italian from './_italian.json';
+import brazilian from './_brazilian.json';
 import german from './_german.json';
+import italian from './_italian.json';
+import russian from './_russian.json';
+import spanish from './_spanish.json';
 
 export const resources = {
   en: english,
-  es: spanish,
-  ru: russian,
-  it: italian,
+  br: brazilian,
   de: german,
+  es: spanish,
+  it: italian,
+  ru: russian,
 };
 
 export const updateLanguage = code => i18next.changeLanguage(code);
